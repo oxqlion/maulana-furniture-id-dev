f78beb917687c187e7e05a4540e652308ee58ff9
Richie Reuben Hermanto <134058136+richiereubenn@users.noreply.github.com>
2023-11-08 18:14:31 +0700

    Halo

--- resources/views/katalog.blade.php
+++ resources/views/katalog.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.header')
 
 @section('content')
-    <body class="pt-28">
+    <body class="pt-28 font-montserrat">
+        <div class="relative -mt-10 mb-8 ">
+            <img src="{{ asset('images/products/header.jpg') }}" alt="Foto Header" class="object-cover w-full h-80" />
+            <div class="absolute inset-0 bg-[#434240] bg-opacity-60"></div>
+            <div class="absolute inset-0 pt-24 lg:pl-24 px-24 text-center lg:text-start" data-aos="fade-right" data-aos-offset="500" data-aos-duration="800">
+                <p class="text-4xl lg:text-6xl font-bold pb-4 text-[#DDBD7E]">
+                    Katalog.
+                </p>
+                <p class="text-xs text-white lg:text-sm font-semibold lg:w-1/2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi placeat corrupti suscipit consectetur quae ullam at quaerat repellendus. Earum enim libero, nihil perferendis culpa commodi incidunt dolores dignissimos eius unde.</p>
+            </div>
+        </div>
         <div class="flex flex-col h-fit container">
             <div class="px-4 mb-3">
                 <div class="flex items-center space-x-2 text-gray-400 text-sm">
