661af60c47551e6f4c4e65554e304c998e0a89ed
oxqlion <rafinaya1127@gmail.com>
2023-12-14 12:48:01 +0700

    aihwda

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', [ProductController::class,'produkPilihan'])->name('home');
+Route::get('/home', [ProductController::class,'produkPilihan'])->name('home');
 
 Route::get('/katalog', [ProductController::class,'listProduk'])->name('katalog');
 
@@ -36,6 +36,6 @@ Route::get('/contact', function () {
 
 Auth::routes();
 
-Route::get('/buat-produk', [ProductController::class,'listProdukAdmin'])->middleware('auth')->name('buat_produk');
+Route::get('/buat-produk', [ProductController::class,'listProdukAdmin'])->middleware('admin')->name('buat_produk');
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
