05e71fb646faefc6ea3e12e569acd2ffeb30d4b2
oxqlion <rafinaya1127@gmail.com>
2023-12-22 19:50:09 +0700

    aowkdoaw

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -94,20 +94,20 @@
             $user = Auth::user();
         @endphp
         @if ($user->role_id == 1)
-        <div class="flex mt-6 gap-4">
-            <h3 class=" text-xl">Projects</h3>
-            <a href="{{ route('tambah_project') }}">
-                <button type="button"
-                    class="flex items-center justify-center text-white bg-[#434240] hover:text-[#DDBD7E] hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
-                    <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true">
-                        <path clip-rule="evenodd" fill-rule="evenodd"
-                            d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
-                    </svg>
-                    Add project
-                </button>
-            </a>
-        </div>
+            <div class="flex mt-6 gap-4">
+                <h3 class=" text-xl">Projects</h3>
+                <a href="{{ route('tambah_project') }}">
+                    <button type="button"
+                        class="flex items-center justify-center text-white bg-[#434240] hover:text-[#DDBD7E] hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+                        <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path clip-rule="evenodd" fill-rule="evenodd"
+                                d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                        </svg>
+                        Add project
+                    </button>
+                </a>
+            </div>
         @endif
         <div class="flex flex-col mt-6">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -165,7 +165,8 @@
                                         </td>
                                         <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                             {{ $pc->deadline }}</td>
-                                        <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
+                                        <td
+                                            class="px-6 py-4 text-sm text-gray-500 flex h-full w-24 truncate overflow-hidden whitespace-nowrap ">
                                             {{ $pc->deskripsi }}</td>
                                         <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
                                             Rp {{ number_format($pc->harga) }}</td>
