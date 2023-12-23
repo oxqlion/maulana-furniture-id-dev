76189e30870d4ae364409739aeef9a2470e17f9c
Richie Reuben Hermanto <134058136+richiereubenn@users.noreply.github.com>
2023-12-23 16:53:32 +0700

    asdhfsak

--- resources/views/tambah_produk.blade.php
+++ resources/views/tambah_produk.blade.php
@@ -1,62 +1,108 @@
 @extends('layouts.sidebar')
 
 @section('content1')
-<section class="bg-white dark:bg-gray-900 overflow-y-scroll">
-    <div class="py-8 px-4 mx-auto max-w-6xl lg:py-8">
-        <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a New Product</h2>
-        <form action="{{ route('simpan_produk') }}"  method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
-                <div class="sm:col-span-2">
-                    <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Produk</label>
-                    <input type="text" name="nama" id="nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Nama produk anda" required="">
+    <section class="bg-white dark:bg-gray-900 overflow-y-scroll">
+        <div class="py-8 px-4 mx-auto max-w-6xl lg:py-8">
+            <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a New Product</h2>
+            @if (session('error'))
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded relative" role="alert">
+                    <strong class="font-bold">Error!</strong>
+                    <span class="block sm:inline">{{ session('error') }}</span>
                 </div>
-                <div>
-                    <label for="kategori" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kategori</label>
-                    <select id="kategori" name="kategori" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                        <option selected="">Pilih Kategori</option>
-                        @foreach ($categories as $category)
-                            <option value="{{ $category->id }}"> {{ $category->nama }}</option>
+            @endif
+            @if ($errors->any())
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <strong class="font-bold">Error!</strong>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
                         @endforeach
-                    </select>
+                    </ul>
                 </div>
-                <div class="w-full">
-                    <label for="kondisi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kondisi</label>
-                    <input type="text" name="kondisi" id="kondisi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Product Kondisi" required="">
+            @endif
+            <form action="{{ route('simpan_produk') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                    <div class="sm:col-span-2">
+                        <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
+                            Produk</label>
+                        <input type="text" name="nama" id="nama"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Nama produk anda" required="">
+                    </div>
+                    <div>
+                        <label for="kategori"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kategori</label>
+                        <select id="kategori" name="kategori"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option selected="">Pilih Kategori</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}"> {{ $category->nama }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="w-full">
+                        <label for="kondisi"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kondisi</label>
+                        <input type="text" name="kondisi" id="kondisi"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Product Kondisi" required="">
+                    </div>
+                    <div class="w-full">
+                        <label for="harga"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga</label>
+                        <input type="number" name="harga" id="harga"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="$2999" required="">
+                    </div>
+                    <div>
+                        <label for="waktu-preorder"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Waktu Preorder</label>
+                        <input type="number" name="waktu_preorder" id="waktu-preorder"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="12" required="">
+                    </div>
+                    <div>
+                        <label for="minimal-pemesanan"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Minimal Pemesanan</label>
+                        <input type="number" name="minimal_pemesanan" id="minimal-pemesanan"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="12" required="">
+                    </div>
+                    <div class="w-full">
+                        <label for="material"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Material</label>
+                        <input type="text" name="material" id="material"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Product brand" required="">
+                    </div>
+                    <div class="w-full">
+                        <label for="furnish"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Furnish</label>
+                        <input type="text" name="furnish" id="furnish"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Product brand" required="">
+                    </div>
+                    <div class="w-full">
+                        <label for="ukuran"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ukuran</label>
+                        <input type="text" name="ukuran" id="ukuran"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Product brand" required="">
+                    </div>
+                    <div class="sm:col-span-2">
+                        <label for="gambar" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gambar
+                            Produk</label>
+                        <input type="file" name="gambar[]" multiple id="gambar" accept="gambar/*"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            multiple required="">
+                    </div>
                 </div>
-                <div class="w-full">
-                    <label for="harga" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga</label>
-                    <input type="number" name="harga" id="harga" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="">
-                </div>
-                <div>
-                    <label for="waktu-preorder" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Waktu Preorder</label>
-                    <input type="number" name="waktu_preorder" id="waktu-preorder" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="12" required="">
-                </div> 
-                <div>
-                    <label for="minimal-pemesanan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Minimal Pemesanan</label>
-                    <input type="number" name="minimal_pemesanan" id="minimal-pemesanan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="12" required="">
-                </div> 
-                <div class="w-full">
-                    <label for="material" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Material</label>
-                    <input type="text" name="material" id="material" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Product brand" required="">
-                </div>
-                <div class="w-full">
-                    <label for="furnish" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Furnish</label>
-                    <input type="text" name="furnish" id="furnish" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Product brand" required="">
-                </div>
-                <div class="w-full">
-                    <label for="ukuran" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ukuran</label>
-                    <input type="text" name="ukuran" id="ukuran" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Product brand" required="">
-                </div>
-                <div class="sm:col-span-2">
-                    <label for="gambar" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Images</label>
-                    <input type="file" name="gambar[]" multiple id="gambar" accept="gambar/*" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" multiple required="">
-                </div>
-            </div>
-            <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-[#434240] hover:bg-[#DDBD7E] transition rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-                Add product
-            </button>
-        </form>
-    </div>
-  </section>
+                <button type="submit"
+                    class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-[#434240] hover:bg-[#DDBD7E] transition rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                    Add product
+                </button>
+            </form>
+        </div>
+    </section>
 @endsection
